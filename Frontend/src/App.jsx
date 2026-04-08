1d25377d982c2db799613cd3e30e6da002b8b0b4
aryan26aa <shivaverma7777@gmail.com>
2026-04-08 17:25:27 +0530

    fix public

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,13 @@
+import { useEffect } from 'react';
 import {RouterProvider} from "react-router"
 import { router } from './app.route.jsx';
 import { AuthProvider } from "./features/auth/auth.context.jsx";
 import { InterviewProvider } from "./features/interview/interview.context.jsx";
 function App() {
+  useEffect(() => {
+    document.title = 'JobAi';
+  }, []);
+
   return (
     <AuthProvider>
 <InterviewProvider>
